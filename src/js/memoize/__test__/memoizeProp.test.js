1dd03d1f922f58d172072ed8d5c1c9724b1fa9f0
Gabirieli Lalasava <gabe@fijiwebdesign.com>
2020-06-14 10:37:02 +0000

    Memoize tets

--- src/js/memoize/__test__/memoizeProp.test.js
+++ src/js/memoize/__test__/memoizeProp.test.js
@@ -6,10 +6,11 @@ const barMemo = memoize(() => Math.random())
 const memoizePropDecorator = (target, propName, descriptor) => {
     const _descripter = Object.getOwnPropertyDescriptor(target, propName)
     console.log('decorator', {target, propName, descriptor, _descripter })
-    const memo = memoize(() => descriptor.get())
+    const { initializer, get } = descriptor
+    const memo = memoize(() => initializer ? initializer()() : (get && get()))
     return {
         get() {
-            console.log('memoizePropDecorator value', memo())
+            console.log('memoizePropDecorator get()', memo())
             return memo()
         } 
     }
@@ -32,7 +33,22 @@ class Model {
         throw Error('Should lazy load')
     }
 
+    @memoizePropDecorator foozz = () => Math.random()
+
+    @memoizePropDecorator nully
+
+    userId = 1
+    
+    @memoizePropDecorator get user() {
+        return new User({ id: this.userId })
+    }
+    
+    // import { belongsTo } from './ModelRelationship'
+    // @belongsTo(User, { foreignKey: 'userId', key: 'id' }) user
+
 }
+
+class User extends Model {}
 
 const memoizeProp = (scope, propName, fn) => {
     const memo = memoize(fn)
@@ -54,11 +70,25 @@ describe('memoizeProp', () => {
         assert(typeof model.foo === 'number')
     })
 
-    it('decorates', () => {
+    it('decorates getter', () => {
         const model = new Model()
         assert(model.fooz === model.fooz)
         console.log('model.fooz', model.fooz)
         assert(typeof model.fooz === 'number')
+    })
+    
+    it('decorates initializer', () => {
+        const model = new Model()
+        assert(model.foozz === model.foozz)
+        console.log('model.foozz', model.foozz)
+        assert(typeof model.foozz === 'number')
+    })
+
+    it('decorates uninitialized', () => {
+        const model = new Model()
+        assert(model.nully === model.nully)
+        console.log('model.nully', model.nully)
+        assert(model.nully === undefined)
     })
 
 })
